d21ba8384cf9b5cbadce772ef544f105a98d918b
mequrel <meq@meq.com>
2015-04-25 23:47:58 +0200

    No user data page added

--- src/main/scala/ztis/recommender/RecommenderPort.scala
+++ src/main/scala/ztis/recommender/RecommenderPort.scala
@@ -1,6 +1,7 @@
 package ztis.recommender
 
 import akka.actor.Actor
+import spray.httpx.marshalling.ToResponseMarshallable
 import spray.routing._
 import spray.http._
 import MediaTypes._
@@ -12,16 +13,8 @@ class RecommenderPortActor extends Actor with RecommenderPort {
   override val recommenderService: RecommenderService = new RecommenderService
 }
 
-
-
 trait RecommenderPort extends HttpService {
   val recommenderService: RecommenderService
-
-  private def toXml(video: Video) = {
-    <li>
-      <a href={ "http://" + video.url }>{video.url}</a>
-    </li>
-  }
 
   val myRoute =
     path("") {
@@ -53,26 +46,56 @@ trait RecommenderPort extends HttpService {
 
           respondWithMediaType(`text/html`) {
             complete {
-              <html>
-                <body>
-                  <h1>User</h1>
-                  <div>
-                    Twitter: {request.twitterId}, Wykop: {request.wykopId}
-                  </div>
-                  <h1>Recommendations:</h1>
-                  <div>
-                    <ul>
-                      {
-                        for { recommendation <- recommendations }
-                          yield toXml(recommendation)
-                      }
-                    </ul>
-                  </div>
-                </body>
-              </html>
+              if(recommendations.isLeft) {
+                presentRecommendations(request, recommendations.left.get)
+              }
+              else {
+                presentNoUserData(request)
+              }
             }
           }
         }
       }
     }
+
+  private def presentRecommendations(request: RecommendRequest, recommendations: List[Video]) = {
+    <html>
+      <body>
+        <h1>User</h1>
+        <div>
+          Twitter: {request.twitterId}, Wykop: {request.wykopId}
+        </div>
+        <h1>Recommendations:</h1>
+        <div>
+          <ul>
+            {
+              for {recommendation <- recommendations}
+                yield toXml(recommendation)
+            }
+          </ul>
+        </div>
+      </body>
+    </html>
+  }
+
+  private def toXml(video: Video) = {
+    <li>
+      <a href={ "http://" + video.url }>{video.url}</a>
+    </li>
+  }
+
+  private def presentNoUserData(request: RecommendRequest) = {
+    <html>
+      <body>
+        <h1>User</h1>
+        <div>
+          Twitter: {request.twitterId}, Wykop: {request.wykopId}
+        </div>
+        <h2>
+          We haven't got any data about the given user. We will try to fetch it soon.
+          Please visit our service tomorrow.
+        </h2>
+      </body>
+    </html>
+  }
 }
--- src/main/scala/ztis/recommender/RecommenderService.scala
+++ src/main/scala/ztis/recommender/RecommenderService.scala
@@ -1,12 +1,21 @@
 package ztis.recommender
 
+import scala.util.Random
+
 class RecommenderService {
-  def recommend(request: RecommendRequest) : List[Video] = {
-    List(
-      Video("youtube.com/watch?v=EWSRbRUH7pg"),
-      Video("youtube.com/watch?v=MEgyGamo79I"),
-      Video("youtube.com/watch?v=sPiWg5jSoZI"),
-      Video("youtube.com/watch?v=0jEmxRJoAtQ")
-    )
+  val random = new Random()
+
+  def recommend(request: RecommendRequest) : Either[List[Video], NoUserData.type] = {
+    if(random.nextBoolean()) {
+      Right(NoUserData)
+    }
+    else {
+      Left(List(
+        Video("youtube.com/watch?v=EWSRbRUH7pg"),
+        Video("youtube.com/watch?v=MEgyGamo79I"),
+        Video("youtube.com/watch?v=sPiWg5jSoZI"),
+        Video("youtube.com/watch?v=0jEmxRJoAtQ")
+      ))
+    }
   }
 }
