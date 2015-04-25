49204763502b5c2131a1d96624fa181acb8cb72c
mequrel <meq@meq.com>
2015-04-25 23:56:16 +0200

    Adding to the queue mock

--- src/main/scala/ztis/recommender/RecommenderService.scala
+++ src/main/scala/ztis/recommender/RecommenderService.scala
@@ -1,15 +1,28 @@
 package ztis.recommender
 
+import com.typesafe.scalalogging.slf4j.StrictLogging
+
 import scala.util.Random
 
-class RecommenderService {
+class RecommenderService extends StrictLogging {
   val random = new Random()
+
+  def scheduleUserDataRetrieval(request: RecommendRequest) = {
+    // TODO: put to the queue
+
+    logger.info(s"Data not found. Scheduled twitter user ${request.twitterId} for high priority data retrieval")
+    logger.info(s"Data not found. Scheduled wykop user ${request.wykopId} for high priority data retrieval")
+  }
 
   def recommend(request: RecommendRequest) : Either[List[Video], NoUserData.type] = {
     if(random.nextBoolean()) {
+      scheduleUserDataRetrieval(request)
       Right(NoUserData)
     }
     else {
+
+      // TODO: fetch from the model
+
       Left(List(
         Video("youtube.com/watch?v=EWSRbRUH7pg"),
         Video("youtube.com/watch?v=MEgyGamo79I"),
