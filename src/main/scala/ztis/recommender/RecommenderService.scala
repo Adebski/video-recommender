324f1da63003c3b4c81abf7ce1c62944f6a7e02c
mequrel <meq@meq.com>
2015-04-25 22:53:51 +0200

    Simple username form.

--- src/main/scala/ztis/recommender/RecommenderService.scala
+++ src/main/scala/ztis/recommender/RecommenderService.scala
@@ -10,6 +10,8 @@ class RecommenderServiceActor extends Actor with RecommenderService {
   def receive = runRoute(myRoute)
 }
 
+case class RecommendRequest(twitterId: String, wykopId: String)
+
 trait RecommenderService extends HttpService {
   val myRoute =
     path("") {
@@ -18,9 +20,33 @@ trait RecommenderService extends HttpService {
           complete {
             <html>
               <body>
-                <h1>Say hello to <i>spray-routing</i> on <i>spray-can</i>!</h1>
+                <form action="/recommend" method="get"> 
+                  <label class="description" for="element_1">Twitter username </label>
+                  <div>
+                    <input id="element_1" name="twitterId" class="element text medium" type="text" maxlength="255" value=""/> 
+                  </div> 
+                  <label class="description" for="element_2">Wykop.pl username </label>
+                  <div>
+                    <input id="element_2" name="wykopId" class="element text medium" type="text" maxlength="255" value=""/> 
+                  </div> 
+                  <input id="saveForm" class="button_text" type="submit" />
+                </form>
               </body>
             </html>
+          }
+        }
+      }
+    } ~ path("recommend") {
+      get {
+        parameters('twitterId.as[String], 'wykopId.as[String]).as(RecommendRequest) { request =>
+          respondWithMediaType(`text/html`) {
+            complete {
+              <html>
+                <body>
+                  Twitter: {request.twitterId}, Wykop: {request.wykopId}
+                </body>
+              </html>
+            }
           }
         }
       }
