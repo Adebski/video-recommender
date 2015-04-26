784362b28551e11c6bd770e71e50c5bd6287e86d
mequrel <meq@meq.com>
2015-04-26 11:22:27 +0200

    Interface and port loaded from config.

--- src/main/scala/ztis/recommender/Recommender.scala
+++ src/main/scala/ztis/recommender/Recommender.scala
@@ -12,12 +12,15 @@ import scala.concurrent.duration._
 object Recommender extends App with StrictLogging {
   val config = ConfigFactory.load("recommender")
 
+  val interface = config.getString("recommender.interface")
+  val port = config.getInt("recommender.port")
+
   implicit val system = ActorSystem("on-spray-can", config)
 
   val service = system.actorOf(Props[RecommenderPortActor], "demo-service")
 
   implicit val timeout = Timeout(5.seconds)
-  IO(Http) ? Http.Bind(service, interface = "localhost", port = 8080)
+  IO(Http) ? Http.Bind(service, interface, port)
 
   logger.info("Recommender service started.")
 }
