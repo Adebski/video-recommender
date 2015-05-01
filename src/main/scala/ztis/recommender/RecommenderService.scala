808ad83429dda44308520a4eaaa0112d100b265b
mequrel <meq@meq.com>
2015-05-02 00:33:19 +0200

    Recommendation for both user ids.

--- src/main/scala/ztis/recommender/RecommenderService.scala
+++ src/main/scala/ztis/recommender/RecommenderService.scala
@@ -1,11 +1,10 @@
 package ztis.recommender
 
-import com.datastax.spark.connector.cql.CassandraConnector
 import com.typesafe.config.{ConfigFactory, Config}
 import com.typesafe.scalalogging.slf4j.StrictLogging
 import org.apache.spark.rdd.RDD
 import org.apache.spark.{SparkContext, SparkConf}
-import org.apache.spark.mllib.recommendation.{Rating, MatrixFactorizationModel}
+import org.apache.spark.mllib.recommendation.MatrixFactorizationModel
 import com.datastax.spark.connector._
 
 import org.apache.spark.SparkContext._
@@ -13,29 +12,58 @@ import scala.collection.JavaConverters._
 
 class RecommenderService extends StrictLogging {
   private val model : MatrixFactorizationModel = fetchModel()
+  private val allUsers: Set[Int] = model.userFeatures.map(_._1).collect().toSet
+
   logger.info("Recommendation model successfully loaded.")
 
-  def scheduleUserDataRetrieval(request: RecommendRequest) = {
-    // TODO: put to the queue
-
-    logger.info(s"Data not found. Scheduled twitter user ${request.twitterId} for high priority data retrieval")
-    logger.info(s"Data not found. Scheduled wykop user ${request.wykopId} for high priority data retrieval")
-  }
-
   def recommend(request: RecommendRequest) : Either[List[Video], NoUserData.type] = {
-    // TODO: get both twitter and wykop ids
+    val usersWithData = filterUsers(request)
 
-    val userId = request.twitterId.toInt
-
-    val recommendation: Array[Rating] = model.recommendProducts(userId, 3)
-
-    if(recommendation.isEmpty) { // TODO: change the condition
-      scheduleUserDataRetrieval(request)
+    if (usersWithData.isEmpty) {
       Right(NoUserData)
     }
     else {
-      Left(recommendation.map(rating => Video(rating.product.toString)).toList)
+      val howMany = 3
+
+      val recommendations = usersWithData.map(userId => model.recommendProducts(userId, howMany))
+        .flatten
+        .sortBy(-_.rating)
+        .map(rating => Video(rating.product.toString))
+        .distinct
+        .take(howMany)
+
+      Left(recommendations)
     }
+  }
+
+  private def filterUsers(request: RecommendRequest) = {
+    var usersWithData: List[Int] = Nil
+
+    val twitterId = request.twitterId.toInt // TODO(#16)
+    if (haveDataFor(twitterId)) {
+      usersWithData = twitterId :: usersWithData
+    }
+    else {
+      // TODO: put to the queue
+
+      logger.info(s"Data not found. Scheduled twitter user ${request.twitterId} for high priority data retrieval")
+    }
+
+    val wykopId = 100 + request.wykopId.toInt // TODO(#16)
+    if (haveDataFor(wykopId)) {
+      usersWithData = twitterId :: usersWithData
+    }
+    else {
+      // TODO: put to the queue
+
+      logger.info(s"Data not found. Scheduled wykop user ${request.wykopId} for high priority data retrieval")
+    }
+
+    usersWithData
+  }
+
+  private def haveDataFor(userId : Int) = {
+    allUsers.contains(userId)
   }
 
   private def fetchModel() : MatrixFactorizationModel = {
