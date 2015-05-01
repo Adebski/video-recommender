597207133ce4a72813929e903eb44848ac15db34
mequrel <meq@meq.com>
2015-05-01 23:05:59 +0200

    Persisting ALS model in the cassandra

--- src/main/scala/ztis/model/ModelBuilder.scala
+++ src/main/scala/ztis/model/ModelBuilder.scala
@@ -1,8 +1,10 @@
 package ztis.model
 
+import com.datastax.spark.connector.cql.CassandraConnector
 import com.typesafe.config.{ConfigFactory, Config}
 import com.typesafe.scalalogging.slf4j.StrictLogging
 import org.apache.spark.mllib.recommendation.{MatrixFactorizationModel, Rating, ALS}
+import org.apache.spark.rdd.RDD
 import org.apache.spark.{SparkConf, SparkContext}
 import com.datastax.spark.connector._
 import org.apache.spark.SparkContext._
@@ -11,7 +13,8 @@ import scala.collection.JavaConverters._
 
 object ModelBuilder extends App with StrictLogging {
   val config = ConfigFactory.load("testdata")
-  val spark = setupSpark(config)
+  val sparkConfig = setupSpark(config)
+  val spark = new SparkContext(sparkConfig)
 
   val ratings = associationRdd.map(row => {
     // temporarily
@@ -34,8 +37,26 @@ object ModelBuilder extends App with StrictLogging {
 
   val model: MatrixFactorizationModel = ALS.train(training, rank, numIters, lambda)
 
+  persistInDatabase(model)
 
   spark.stop()
+
+  private def persistInDatabase(model: MatrixFactorizationModel) = {
+    val keyspace = config.getString("cassandra.keyspace")
+    val userFeaturesTable = "modelUserFeatures"
+    val productFeaturesTable = "modelProductFeatures"
+
+    saveFeatureRdd(keyspace, userFeaturesTable, model.userFeatures)
+    saveFeatureRdd(keyspace, productFeaturesTable, model.productFeatures)
+  }
+
+  private def saveFeatureRdd(keyspace: String, tableName: String, rdd: RDD[(Int, Array[Double])]) = {
+    CassandraConnector(sparkConfig).withSessionDo { session =>
+      session.execute(s""" DROP TABLE IF EXISTS $keyspace."$tableName" """)
+    }
+
+    rdd.map(feature => (feature._1, feature._2.toList)).saveAsCassandraTable(keyspace, tableName)
+  }
 
   // TODO(#8): extract to CassandraClient or sth like this
   private def associationRdd = {
@@ -46,16 +67,16 @@ object ModelBuilder extends App with StrictLogging {
   }
 
   // TODO(#8): copied from MovieLensDataLoader, adhere to DRY,
-  private def setupSpark(config: Config): SparkContext = {
+  private def setupSpark(config: Config) = {
     val cassandraHost = config.getStringList("cassandra.contact-points").asScala.iterator.next().split(":")(0)
 
     val sparkConfig = new SparkConf()
       .setMaster(s"local[2]")
-      .setAppName("ModelValidator")
+      .setAppName("ModelBuilder")
       .set("spark.executor.memory", "8g")
       .set("spark.cassandra.connection.host", cassandraHost)
 
-    new SparkContext(sparkConfig)
+    sparkConfig
   }
 
 }
