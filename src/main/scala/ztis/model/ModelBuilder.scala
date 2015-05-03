07abbb60da997966d6788004ed4675bad256c020
mequrel <meq@meq.com>
2015-05-03 16:16:43 +0200

    Report directory and file

--- src/main/scala/ztis/model/ModelBuilder.scala
+++ src/main/scala/ztis/model/ModelBuilder.scala
@@ -9,7 +9,10 @@ import org.apache.spark.{SparkConf, SparkContext}
 import com.datastax.spark.connector._
 import org.apache.spark.SparkContext._
 import org.apache.spark.mllib.evaluation.{RegressionMetrics, BinaryClassificationMetrics}
+import org.joda.time.DateTime
+import org.joda.time.format.DateTimeFormat
 
+import scala.reflect.io.File
 import scala.sys.process._
 import scala.collection.JavaConverters._
 
@@ -40,52 +43,66 @@ object ModelBuilder extends App with StrictLogging {
   val lambda = 0.1
   val numIters = 10
 
-  val model: MatrixFactorizationModel = ALS.train(training, rank, numIters, lambda)
+  val model = ALS.train(training, rank, numIters, lambda)
 
-  val userProducts = validation.map(rating => (rating.user, rating.product))
-  val predictions = model.predict(userProducts)
-  val keyedPredictions = predictions.map(rating => ((rating.user, rating.product), rating.rating))
-  val keyedObservations = validation.map(rating => ((rating.user, rating.product), rating.rating))
-  val predictionAndObservations = keyedPredictions.join(keyedObservations).values
+  val directory = s"report-${DateTime.now().toString(DateTimeFormat.forPattern("yyyy-MM-dd--HH-mm-ss"))}"
+  s"mkdir $directory" !!
 
-  val regressionMetrics = new RegressionMetrics(predictionAndObservations)
-  val rmse = regressionMetrics.rootMeanSquaredError
-  logger.info(s"RMSE: $rmse")
-
-  def isGoodEnough(observation: Double) = if (observation > 3) 1.0 else 0.0
-
-  val scoresAndLabels = predictionAndObservations.map {
-    case (prediction, observation) => (prediction, isGoodEnough(observation))
-  }
-
-  scoresAndLabels.cache()
-  val fraction = 1000.0 / scoresAndLabels.count()
-
-  // add naive classifier for comparsion, all scores are mean of ratings for a product
-
-  val metrics = new BinaryClassificationMetrics(scoresAndLabels)
-  val prArea = metrics.areaUnderPR()
-  val rocArea = metrics.areaUnderROC()
-
-  "rm -r pr.dat roc.dat thresholds.dat" !!
-
-  metrics.pr().map {
-    case (recall, precision) => s"$recall, $precision"
-  }.sample(withReplacement = false, fraction).saveAsTextFile("pr.dat")
-
-  metrics.roc().map {
-    case (rate1, rate2) => s"$rate1, $rate2"
-  }.sample(withReplacement = false, fraction).saveAsTextFile("roc.dat")
-
-  metrics.precisionByThreshold().join(metrics.recallByThreshold()).join(metrics.fMeasureByThreshold()).map {
-    case (threshold, ((precision, recall), fMeasure)) => s"$threshold, $precision, $recall, $fMeasure"
-  }.sample(withReplacement = false, fraction).saveAsTextFile("thresholds.dat")
-
-  logger.info(s"area under PR: $prArea, area under ROC: $rocArea")
+  val score = evaluate(model, directory)
 
   persistInDatabase(model)
 
   spark.stop()
+
+  def evaluate(model: MatrixFactorizationModel, reportDirectory: String) = {
+    val userProducts = validation.map(rating => (rating.user, rating.product))
+    val predictions = model.predict(userProducts)
+    val keyedPredictions = predictions.map(rating => ((rating.user, rating.product), rating.rating))
+    val keyedObservations = validation.map(rating => ((rating.user, rating.product), rating.rating))
+    val predictionAndObservations = keyedPredictions.join(keyedObservations).values
+
+    val regressionMetrics = new RegressionMetrics(predictionAndObservations)
+    val rmse = regressionMetrics.rootMeanSquaredError
+
+    def isGoodEnough(observation: Double) = if (observation > 3) 1.0 else 0.0
+
+    val scoresAndLabels = predictionAndObservations.map {
+      case (prediction, observation) => (prediction, isGoodEnough(observation))
+    }
+
+    scoresAndLabels.cache()
+    val fraction = 1000.0 / scoresAndLabels.count()
+
+    // add naive classifier for comparsion, all scores are mean of ratings for a product
+
+    val metrics = new BinaryClassificationMetrics(scoresAndLabels)
+    val prArea = metrics.areaUnderPR()
+    val rocArea = metrics.areaUnderROC()
+
+    metrics.pr().map {
+      case (recall, precision) => s"$recall, $precision"
+    }.sample(false, fraction).saveAsTextFile(directory + "/pr.dat")
+
+    metrics.roc().map {
+      case (rate1, rate2) => s"$rate1, $rate2"
+    }.sample(false, fraction).saveAsTextFile(directory + "/roc.dat")
+
+    metrics.precisionByThreshold().join(metrics.recallByThreshold()).join(metrics.fMeasureByThreshold()).map {
+      case (threshold, ((precision, recall), fMeasure)) => s"$threshold, $precision, $recall, $fMeasure"
+    }.sample(false, fraction).saveAsTextFile(directory + "/thresholds.dat")
+
+    val report =
+      s"""
+        |RMSE: $rmse
+        |AUC PR: $prArea
+        |AUC ROC: $rocArea
+      """.stripMargin
+
+    logger.info(report)
+    File(directory + "/report.txt").writeAll(report)
+
+    rocArea
+  }
 
   private def persistInDatabase(model: MatrixFactorizationModel) = {
     val keyspace = config.getString("cassandra.keyspace")
