a56acb4ec42db507a089aadf1d67a86bb2c9fddb
mequrel <meq@meq.com>
2015-05-03 14:57:15 +0200

    RMSE from MLLib

--- src/main/scala/ztis/model/ModelBuilder.scala
+++ src/main/scala/ztis/model/ModelBuilder.scala
@@ -8,8 +8,9 @@ import org.apache.spark.rdd.RDD
 import org.apache.spark.{SparkConf, SparkContext}
 import com.datastax.spark.connector._
 import org.apache.spark.SparkContext._
-import org.apache.spark.mllib.evaluation.BinaryClassificationMetrics
+import org.apache.spark.mllib.evaluation.{RegressionMetrics, BinaryClassificationMetrics}
 
+import scala.sys.process._
 import scala.collection.JavaConverters._
 
 object ModelBuilder extends App with StrictLogging {
@@ -18,7 +19,7 @@ object ModelBuilder extends App with StrictLogging {
   val spark = new SparkContext(sparkConfig)
 
   val ratings = associationRdd.map(row => {
-    // temporarily
+    // TODO(#16)
     val userId = row.getString("user_id").toInt
     val link = row.getString("link").toInt
     val rating = row.getInt("rating")
@@ -34,45 +35,51 @@ object ModelBuilder extends App with StrictLogging {
 
   logger.info(s"Count: ${training.count()}\n")
 
+  // TODO(#13)
   val rank = 8
   val lambda = 0.1
   val numIters = 10
 
   val model: MatrixFactorizationModel = ALS.train(training, rank, numIters, lambda)
 
-//  val validationRmse = computeRmse(model, validation)
-//  logger.info(s"RMSE: $validationRmse")
-
-  // regressoinMetrics!
-
   val userProducts = validation.map(rating => (rating.user, rating.product))
-  val scores = model.predict(userProducts)
+  val predictions = model.predict(userProducts)
+  val keyedPredictions = predictions.map(rating => ((rating.user, rating.product), rating.rating))
+  val keyedObservations = validation.map(rating => ((rating.user, rating.product), rating.rating))
+  val predictionAndObservations = keyedPredictions.join(keyedObservations).values
 
-  def toDouble(boolean: Boolean) = if(boolean) 1.0 else 0.0
-  val labels = validation.map(rating => rating.copy(rating = toDouble(rating.rating >= 4)))
+  val regressionMetrics = new RegressionMetrics(predictionAndObservations)
+  val rmse = regressionMetrics.rootMeanSquaredError
+  logger.info(s"RMSE: $rmse")
 
-  val keyedScores = scores.map(rating => ((rating.user, rating.product), rating.rating))
-  val keyedLabels = labels.map(rating => ((rating.user, rating.product), rating.rating))
-  val scoresAndLabels = keyedScores.join(keyedLabels).values
+  def isGoodEnough(observation: Double) = if (observation > 3) 1.0 else 0.0
+
+  val scoresAndLabels = predictionAndObservations.map {
+    case (prediction, observation) => (prediction, isGoodEnough(observation))
+  }
+
   scoresAndLabels.cache()
   val fraction = 1000.0 / scoresAndLabels.count()
 
+  // add naive classifier for comparsion, all scores are mean of ratings for a product
 
   val metrics = new BinaryClassificationMetrics(scoresAndLabels)
   val prArea = metrics.areaUnderPR()
   val rocArea = metrics.areaUnderROC()
 
+  "rm -r pr.dat roc.dat thresholds.dat" !!
+
   metrics.pr().map {
     case (recall, precision) => s"$recall, $precision"
-  }.sample(false, fraction).saveAsTextFile("pr.dat")
+  }.sample(withReplacement = false, fraction).saveAsTextFile("pr.dat")
 
   metrics.roc().map {
     case (rate1, rate2) => s"$rate1, $rate2"
-  }.sample(false, fraction).saveAsTextFile("roc.dat")
+  }.sample(withReplacement = false, fraction).saveAsTextFile("roc.dat")
 
   metrics.precisionByThreshold().join(metrics.recallByThreshold()).join(metrics.fMeasureByThreshold()).map {
     case (threshold, ((precision, recall), fMeasure)) => s"$threshold, $precision, $recall, $fMeasure"
-  }.sample(false, fraction).saveAsTextFile("thresholds.dat")
+  }.sample(withReplacement = false, fraction).saveAsTextFile("thresholds.dat")
 
   logger.info(s"area under PR: $prArea, area under ROC: $rocArea")
 
@@ -97,16 +104,8 @@ object ModelBuilder extends App with StrictLogging {
     rdd.map(feature => (feature._1, feature._2.toList)).saveAsCassandraTable(keyspace, tableName)
   }
 
-  private def computeRmse(model: MatrixFactorizationModel, data: RDD[Rating]) = {
-    val predictions: RDD[Rating] = model.predict(data.map(x => (x.user, x.product)))
-    val predictionsAndRatings = predictions.map(x => ((x.user, x.product), x.rating)).join(
-      data.map(x => ((x.user, x.product), x.rating))).values
-
-    math.sqrt(predictionsAndRatings.map(x => (x._1 - x._2) * (x._1 - x._2)).reduce(_ + _) / data.count())
-  }
-
   // TODO(#8): extract to CassandraClient or sth like this
- private def associationRdd = {
+  private def associationRdd = {
     val keyspace = config.getString("cassandra.keyspace")
     val explicitAssocTableName = config.getString("cassandra.explicit-association-table-name")
 
